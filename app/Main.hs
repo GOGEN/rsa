78b5bc627540d167a5a853a711e12eef2efa3c27
Latypov Marat <gogen.marat@gmail.com>
2015-09-27 20:14:17 +0300

    реализовал генерацию простых чисел и перенес модули в папку проекта

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,14 @@ module Main where
 import Lib
 import Euclid
 import FastDegree
+import GeneratePrimeNumber
+import System.Random
 
 main :: IO ()
 main = do
   print $ euc 172 38
   print $ deg 19 5 13
+  g <- getStdGen
+  rank <- getLine
+  let (r, g') = genPrime g (read rank :: Integer)
+  print r
